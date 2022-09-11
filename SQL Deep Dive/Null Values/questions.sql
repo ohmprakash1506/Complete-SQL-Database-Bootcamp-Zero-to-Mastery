f16cdc388e6eb12ec82497cc2b9a785f553a1139
OHM PRAKASH <48152924+ohmprakash1506@users.noreply.github.com>
2022-09-11 18:46:42 +0530

    Update questions.sql

--- SQL Deep Dive/Null Values/questions.sql
+++ SQL Deep Dive/Null Values/questions.sql
@@ -3,10 +3,10 @@
 * DB: https://www.db-fiddle.com/f/PnGNcaPYfGoEDvfexzEUA/0
 * Question: 
 * Assuming a students minimum age for the class is 15, what is the average age of a student?
-*/
+*/SELECT avg(coalesce(age, 15)) FROM Students;
 
 /*
 * DB: https://www.db-fiddle.com/f/PnGNcaPYfGoEDvfexzEUA/0
 * Question: 
 * Replace all empty first or last names with a default?
-*/
+*/SELECT id, coalesece(firstname, 'firstname'), coalesce(lastname, 'lastname'), age from Students;
